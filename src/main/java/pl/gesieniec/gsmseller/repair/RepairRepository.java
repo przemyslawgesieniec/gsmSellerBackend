e4deae393ba09cd9db7ed332bd2bd7ebcc3de6d4
przemyslawgesieniecasit <przemyslaw.gesieniec@gmail.com>
2026-01-30 19:21:30 +0100

    service draft

---
+++ src/main/java/pl/gesieniec/gsmseller/repair/RepairRepository.java
@@ -0,0 +1,11 @@
+package pl.gesieniec.gsmseller.repair;
+
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepairRepository extends JpaRepository<Repair, Long> {
+    Optional<Repair> findByTechnicalId(UUID technicalId);
+}
