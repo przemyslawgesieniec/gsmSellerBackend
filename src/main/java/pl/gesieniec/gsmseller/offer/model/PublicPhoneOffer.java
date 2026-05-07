590c7508855ae394f660c000d31479ecd8d06f54
przemyslawgesieniecasit <przemyslaw.gesieniec@gmail.com>
2026-05-08 00:17:24 +0200

    pagination fixed on loadModels function

---
+++ src/main/java/pl/gesieniec/gsmseller/offer/model/PublicPhoneOffer.java
@@ -0,0 +1,32 @@
+package pl.gesieniec.gsmseller.offer.model;
+
+import lombok.Builder;
+import pl.gesieniec.gsmseller.offer.model.specs.CommunicationSpecs;
+import pl.gesieniec.gsmseller.offer.model.specs.ScreenSpecs;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+public record PublicPhoneOffer(
+    UUID technicalId,
+    BigDecimal price,
+    String brand,
+    String model,
+    String status,
+    String color,
+    String location,
+    ScreenSpecs screen,
+    String memory,
+    String ram,
+    String simCardType,
+    List<Integer> frontCamerasMpx,
+    List<Integer> backCamerasMpx,
+    String batteryCapacity,
+    String batteryCondition,
+    CommunicationSpecs communication,
+    String operatingSystem,
+    List<Photo> photos,
+    Boolean isReserved
+) {}
